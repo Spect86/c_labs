60f0e9f934a54efd16d4b1fb6fc490492fb1fda1
KonurbaevaTN <konurbaeva_tn@edu.surgu.ru>
2022-10-19 13:58:29 +0500

    add lab

---
+++ lab3/lab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x;
+    printf("Enter x { -1.0 < x < 1.0 } -> ");
+    scanf("%lf", &x);
+    if (-1.0 < x && x < 1.0)
+    {
+        double y = log((1+x)/(1-x));
+        double z = (y*y)/(1+y);
+        printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+    }
+    else
+        printf("x value is incorrect!\n");
+
+    return 0;
+}
