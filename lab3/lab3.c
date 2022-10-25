11f167075cbc35454be2fef1518dccafb997a532
Spect86 <112753079+Spect86@users.noreply.github.com>
2022-10-25 18:28:31 +0500

    Add files via upload

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -11,17 +11,17 @@ int main()
 
    while (0 <= x && x <= 2)
    {
-      if (0 <= x && x <= 1)
+      for (x; 0 <= x && x <= 1; x+h)
       {
          printf("%lf", x);
          double f = (cos(x + pow(x, 3)));
          printf("            %lf \n \n", f);
          x += h;
       }
-      else if (1 < x && x <= 2)
+      for (x; 1 < x && x <= 2; x+h)
       {
          printf("%lf", x);
-         double f = ((exp(-1*pow(x, 2)))- (pow(x, 2)) + 2*x);
+         double f = ((exp(-1 * pow(x, 2))) - (pow(x, 2)) + 2 * x);
          printf("            %lf \n \n", f);
          x += h;
       }
